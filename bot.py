e21bd35a04ccf06a2429dcac81736d2ada40513c
Sithell <Sagitavkhas2003@gmail.com>
2017-05-21 20:54:17 +0300

    You should not care about it

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from engine import Engine
 from telebot import TeleBot, types
-import threading
-import time
+# import threading
+# import time
 
 token = '396661224:AAEhmcS8sfEL7dq9892WNUOWTWWGCyfNKDw'
 bot = TeleBot(token)
-result = None
+
 
 @bot.message_handler(commands=["start", "info"])
 def info(message):
@@ -15,18 +15,17 @@ def info(message):
     btn3 = types.KeyboardButton("Программист Unity")
     btn4 = types.KeyboardButton("Web-дизайнер")
     markup.add(btn1, btn2, btn3, btn4)
-    bot.send_message(message.chat.id, "Введите название специальности или выберите из списка:", reply_markup=markup)
+    _ = "Введите название специальности или выберите из списка:"
+    bot.send_message(message.chat.id, _, reply_markup=markup)
 
 
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
-    global result
-    threading.Timer(0, play_animation, args=[message.chat.id]).start()
-    print(message.text)
+    # threading.Timer(0, play_animation, args=[message.chat.id]).start()
+    print("recieved message")
+    bot.send_message(message.chat.id, "Подождите...")
     engine = Engine()
     result = engine.parser(message.text)
-    print(result)
-    print()
 
     for item in result:  # pretty print
         percent = str(round(item[1] * 100)) + '%'
@@ -35,25 +34,24 @@ def message_handler(message):
 
         bot.send_message(message.chat.id, answer)
 
-    result = None
     del engine
 
 
-def play_animation(chat_id):
-    global result
-    message_id = bot.send_message(chat_id, "Подождите...").message_id
-    time.sleep(0.25)
-    while not result:
-        bot.edit_message_text("Подождите", chat_id, message_id)
-        time.sleep(0.25)
-        bot.edit_message_text("Подождите.", chat_id, message_id)
-        time.sleep(0.25)
-        bot.edit_message_text("Подождите..", chat_id, message_id)
-        time.sleep(0.25)
-        bot.edit_message_text("Подождите...", chat_id, message_id)
-        time.sleep(0.25)
-
-    bot.edit_message_text("Готово!", chat_id, message_id)
+# def play_animation(chat_id):
+#     global result
+#     message_id = bot.send_message(chat_id, "Подождите...").message_id
+#     time.sleep(0.25)
+#     while not result:
+#         bot.edit_message_text("Подождите", chat_id, message_id)
+#         time.sleep(0.25)
+#         bot.edit_message_text("Подождите.", chat_id, message_id)
+#         time.sleep(0.25)
+#         bot.edit_message_text("Подождите..", chat_id, message_id)
+#         time.sleep(0.25)
+#         bot.edit_message_text("Подождите...", chat_id, message_id)
+#         time.sleep(0.25)
+#
+#     bot.edit_message_text("Готово!", chat_id, message_id)
 
 
 if __name__ == '__main__':
