699400e165f3d362895a9090ff10865f41bba52b
Sithell <Sagitavkhas2003@gmail.com>
2017-05-21 20:05:40 +0300

    animation fixed

--- bot.py
+++ bot.py
@@ -5,7 +5,9 @@ import time
 
 token = '396661224:AAEhmcS8sfEL7dq9892WNUOWTWWGCyfNKDw'
 bot = TeleBot(token)
+event = threading.Event()
 result = None
+
 
 @bot.message_handler(commands=["start", "info"])
 def info(message):
@@ -20,7 +22,7 @@ def info(message):
 
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
-    global result
+    global result, event
     threading.Timer(0, play_animation, args=[message.chat.id]).start()
     print("message received")
     engine = Engine()
@@ -33,15 +35,16 @@ def message_handler(message):
 
         bot.send_message(message.chat.id, answer)
 
-    result = None
+    event.set()
     del engine
 
 
 def play_animation(chat_id):
-    global result
+    global event, result
     message_id = bot.send_message(chat_id, "Подождите...").message_id
     time.sleep(0.25)
-    while not result:
+
+    while not event.is_set():
         bot.edit_message_text("Подождите", chat_id, message_id)
         time.sleep(0.25)
         bot.edit_message_text("Подождите.", chat_id, message_id)
@@ -52,6 +55,7 @@ def play_animation(chat_id):
         time.sleep(0.25)
 
     bot.edit_message_text("Готово!", chat_id, message_id)
+    event.clear()
 
 
 if __name__ == '__main__':
